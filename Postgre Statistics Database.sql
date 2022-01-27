c17273752757f648cd7f2a56fd2bd346755c4393
Agbaeze David 17CJ022467 <75173110+davidagbaeze@users.noreply.github.com>
2022-01-27 21:22:01 +0100

    Delete Postgre Statistics Database.sql

--- Postgre Statistics Database.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Public."Statistics"(Country varchar(100), Years int, Area varchar(100), Sex varchar(100), Citizenship varchar(100), Statustimeofdeparture varchar(200), Record varchar(100), Relaiability varchar(100), SourceYear int, TheValue bigint)
-
-copy Public."Statistics" FROM 'C:\Users\DELL\Statistics.csv' DELIMITER ',' CSV HEADER;
-
-select * from Public."Statistics"
